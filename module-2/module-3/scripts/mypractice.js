08d88b9541ff477dc71dc9ead801875081297b4e
sam smitch <rabihussain16@gmail.com>
2020-11-25 13:25:48 +0000

    task-3.2.4.html
    if condition

---
+++ module-2/module-3/scripts/mypractice.js
@@ -0,0 +1,16 @@
+function ReadStudentMarks () {
+var R = document.getElementById(Rabi).value;
+console.log('Rabi mark :' +  R);
+var I = document.getElementById(irfan).value;
+console.log('Irfan mark: '+ I);
+var M = document.getElementById(mar).value;
+console.log('Maryam mark: '+M);
+var A = document.getElementById(afrah).value;
+console.log('Afrah Mar: '+A);
+var H = document.getElementById(hus).value;
+console.log('hus Mar: '+H);
+}
+
+function myFunction() {
+    document.getElementById("field2").value = document.getElementById("field1").value;
+  }
---
+++ module-2/module-3/scripts/task-3.2.4.js
@@ -0,0 +1,5 @@
+var temperature=40;
+
+if(temperature < 50){
+    console.log('please put on your coat');
+}
