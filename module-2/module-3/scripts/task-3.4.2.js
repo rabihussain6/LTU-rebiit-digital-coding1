4e2f8d61153f6d66875517c20d358d837917939e
sam smitch <rabihussain16@gmail.com>
2020-11-26 11:33:24 +0000

    task-3.4.4.js

---
+++ module-2/module-3/scripts/task-3.4.2.js
@@ -0,0 +1,6 @@
+var favthings=['waffels','egglesscake','jamun'];
+
+
+for(var i=0;  i<favthings.length;  i++) {
+    console.log( favthings[i] );
+}
---
+++ module-2/module-3/scripts/task-3.4.4.js
@@ -0,0 +1,20 @@
+var dinner={
+    recipeTitle:'chickenpasta',
+    serving:4,
+    ingredients:['500g chicken','pasta','oragano','1 tbsp olive oil'],
+    direction:'rabirestaurant'
+}
+//console.log(  dinner.recipeTitle);
+//console.log(serving);
+//console.log(ingredients);
+//console.console.log(direction);
+
+
+var ingredients=dinner.ingredients;
+
+
+for(var i=0;i< ingredients.length;i++){
+
+
+    console.log(ingredients[i]);
+}
