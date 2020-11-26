5b1337300caabbb8a1ffb028145d1b49ab3b531b
sam smitch <rabihussain16@gmail.com>
2020-11-26 10:17:37 +0000

    task-3.4.1.html
    js for loop

---
+++ module-2/module-3/scripts/task-3.2.5.js
@@ -0,0 +1,18 @@
+var degrees=29;
+
+if(degrees >=50){
+
+    console.log('wear a coat');
+
+} else if(degrees<30) {
+
+    console.log('wear coat and hat');
+
+} else if(degrees<0){
+
+
+    console.log('stai inside');
+
+}else{
+    console.log('Otherwise, just pants and vest is fine');
+}
---
+++ module-2/module-3/scripts/task-3.4.1.js
@@ -0,0 +1,6 @@
+for(var i=1; i <=12; i++){
+
+    var number=i*7;
+
+    console.log('7' + '*' + i + '=' + number);
+}
