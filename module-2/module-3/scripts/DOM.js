cc38faf118715a11ec825592fea3c42704613e3c
sam smitch <rabihussain16@gmail.com>
2020-11-30 10:51:58 +0000

    index.html

---
+++ module-2/module-3/scripts/DOM.js
@@ -0,0 +1,13 @@
+/*document.querySelector('h1');*/
+let heading1= document.querySelector('h1');
+
+//console.log( heading1);
+
+let paragraph = document.querySelector('p');
+//console.log( paragraph);
+
+let listitem= document.querySelectorAll('li');
+
+//console.log(listitem);
+let itemclass = document.getElementByIdclassName('item');
+console.log (itemclass);
